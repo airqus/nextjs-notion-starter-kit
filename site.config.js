f07b9b794432bed1c6ca5483be8fdf2c98d973cc
airqus <62753035+airqus@users.noreply.github.com>
2022-03-31 01:54:53 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,23 +1,23 @@
 export default {
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '695748ebd6cc4263ba43bb0bfdc706f2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Ariff Rashid',
+  domain: 'ariffrashid.me',
+  author: 'Ariff Rashid',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Ariff Rashid',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'airqus',
+  github: 'airqus',
+  linkedin: 'ariffrashid',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
